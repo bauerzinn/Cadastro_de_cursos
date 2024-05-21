d4cf23eb096637aa0b8b58899580e5d4d31b2443
Gabriel Bauer <gabbauer1218@gmail.com>
2024-05-21 14:17:39 -0300

    Interface do site
    90% da interface, colocarei um botão onde ele puxara os dados para o sql server

--- cadastro.py
+++ cadastro.py
@@ -23,3 +23,19 @@ def inserir_dados(nome, curso, hrs_curso, valor_curso):
     cursor.commit()
     cursor.close()
     conexao.close()
+
+
+#interface
+
+image = open('iclogo.png', 'rb').read()
+width = 100
+
+st.image(image,
+        caption='',
+        width=width,
+)
+
+nome = st.text_input("Nome:")
+curso = st.selectbox("Curso:", options = ['---', 'Agronomia', 'Administração e Gestão', 'Antropologia', 'Arquitetura', 'Artes Cênicas', 'Artes Plásticas', 'Astronomia', 'Biologia', 'Bioquímica', 'Ciência de Dados e IA', 'Ciência Política', 'Ciências Ambientais', 'Ciências Sociais', 'Comunicação Social', 'Contabilidade', 'Design de Interiores', 'Design de Moda', 'Desenvolvimento de Software', 'Desenvolvimento Web', 'Direito', 'Economia', 'Educação Física', 'Enfermagem', 'Engenharia Aeroespacial', 'Engenharia Civil', 'Engenharia Elétrica', 'Engenharia Mecânica', 'Engenharia Química', 'Farmácia', 'Filosofia', 'Finanças', 'Fisioterapia', 'Física', 'Geografia', 'Geologia', 'Gestão de Recursos Humanos', 'História', 'Jornalismo', 'Letras', 'Línguas', 'Marketing', 'Marketing Digital', 'Matemática', 'Medicina', 'Medicina Veterinária', 'Música', 'Nutrição', 'Odontologia', 'Pedagogia', 'Psicologia', 'Publicidade e Propaganda', 'Química', 'Relações Internacionais', 'Segurança da Informação', 'Serviço Social', 'Sociologia', 'Teologia', 'Turismo'])
+hrs_curso = st.number_input("Horas do Curso:", format="%d", step=1)
+valor_curso = st.selectbox("Valor do Curso:", options = ['Nada', 'de 0 a 100 R$', '100 a 250 R$', '250 a 500 R$', 'mais de 500 R$'])
