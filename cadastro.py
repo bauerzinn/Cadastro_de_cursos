e7d838e46a5bb6251514338976c141315f14e3a9
Gabriel Bauer <gabbauer1218@gmail.com>
2024-05-22 16:55:01 -0300

    Update cadastro
    Adicionei um botâo que manda os dados para o sql server e um outro botão para visualização dos cadastros dentro da pagina...

--- cadastro.py
+++ cadastro.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 import pyodbc
+
+def app():
+    st.write('cadastro')
 
 #conexao banco de dados
 def conectar_bd():
@@ -39,3 +43,32 @@ nome = st.text_input("Nome:")
 curso = st.selectbox("Curso:", options = ['---', 'Agronomia', 'Administração e Gestão', 'Antropologia', 'Arquitetura', 'Artes Cênicas', 'Artes Plásticas', 'Astronomia', 'Biologia', 'Bioquímica', 'Ciência de Dados e IA', 'Ciência Política', 'Ciências Ambientais', 'Ciências Sociais', 'Comunicação Social', 'Contabilidade', 'Design de Interiores', 'Design de Moda', 'Desenvolvimento de Software', 'Desenvolvimento Web', 'Direito', 'Economia', 'Educação Física', 'Enfermagem', 'Engenharia Aeroespacial', 'Engenharia Civil', 'Engenharia Elétrica', 'Engenharia Mecânica', 'Engenharia Química', 'Farmácia', 'Filosofia', 'Finanças', 'Fisioterapia', 'Física', 'Geografia', 'Geologia', 'Gestão de Recursos Humanos', 'História', 'Jornalismo', 'Letras', 'Línguas', 'Marketing', 'Marketing Digital', 'Matemática', 'Medicina', 'Medicina Veterinária', 'Música', 'Nutrição', 'Odontologia', 'Pedagogia', 'Psicologia', 'Publicidade e Propaganda', 'Química', 'Relações Internacionais', 'Segurança da Informação', 'Serviço Social', 'Sociologia', 'Teologia', 'Turismo'])
 hrs_curso = st.number_input("Horas do Curso:", format="%d", step=1)
 valor_curso = st.selectbox("Valor do Curso:", options = ['Nada', 'de 0 a 100 R$', '100 a 250 R$', '250 a 500 R$', 'mais de 500 R$'])
+
+
+if st.button("Inserir"):
+    if nome and curso and hrs_curso and valor_curso:
+        try:
+            inserir_dados(nome, curso, hrs_curso, valor_curso)
+            st.success("Dados inseridos com sucesso!")
+        except Exception as e:
+            st.error(f"Erro ao inserir dados: {e}")
+    else:
+        st.warning("Por favor, preencha todos os campos.")
+
+
+def buscar_dados():
+    conexao = conectar_bd()
+    cursor = conexao.cursor()
+    comando = "SELECT * FROM cadastro"
+    cursor.execute(comando)
+    data = cursor.fetchall()
+    cursor.close()
+    conexao.close()
+    return data
+
+if st.button("Visualizar pessoas cadastradas"):
+    try:
+        data = buscar_dados()
+        st.write(data)
+    except Exception as e:
+        st.error(f"Erro ao visualizar dados: {e}")
