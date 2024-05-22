292b4604922b506ebee89f6c2dfd87f0b43aafd9
Gabriel Bauer <gabbauer1218@gmail.com>
2024-05-22 17:06:21 -0300

    Create sql
    Coloquei o código sql server que usei para formar o banco de dados do site...

---
+++ index.sql
@@ -0,0 +1,12 @@
+--criando database--
+create database IC
+
+--criando tabelas dentro do database--
+Use IC
+create table cadastro(
+    nome varchar (50),
+    curso varchar (50),
+    hrs_curso int,
+    valor_curso varchar(50)
+)
+
